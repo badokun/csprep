1787cb975f1c163049226d8eba3505bf010ab19d
Henno van Rensburg <henno_vanrensburg@monex.co.jp>
2015-10-22 17:15:10 +0900

    removed old index.js

--- rest-api/newindex.js
+++ rest-api/newindex.js
@@ -1,3 +1,9 @@
+/*
+    Start up MongoDB 
+    C:\Program Files\MongoDB\Server\3.0\bin>mongod.exe --dbpath C:\src\csprep\rest-api\MongoDB
+*/
+
+
 /* eslint-env node */
 /* jshint node: true */
 (function () {
